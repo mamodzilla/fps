7715e29fcac7ebbb5cb5d1d7975332c5932c1238
yuripavlowww <yuripavlowww@gmail.com>
2024-03-26 21:40:05 +0300

    feat: add test objects and realize simple dash logic

--- Assets/Code/Scripts/Hero/Movement.cs
+++ Assets/Code/Scripts/Hero/Movement.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEditor;
 //using UnityEditor.U2D;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
     
     private float speed = 6.0f;
     private CharacterController characterController;
-    private float gravity = -9.8f; 
+    private float gravity = -9.8f;
 
+    private float clickTime;
+
+    private KeyCode previousKey;
+
+    public float dashTime = 0.5f;
+    public float dashDistance = 5f;
 
     private void Start()
     {
@@ -17,12 +27,26 @@ public class Movement : MonoBehaviour
     }
     private void Update()
     {
-        if(Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = 10.0f;
         } else {
             speed = 6.0f;
         }
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Dash(KeyCode.W, new Vector3(0, 0, dashDistance));
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            Dash(KeyCode.A, new Vector3(-dashDistance, 0, 0));
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            Dash(KeyCode.S, new Vector3(0, 0, -dashDistance));
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Dash(KeyCode.D, new Vector3(dashDistance, 0, 0));
+        }
+
         float deltaX = Input.GetAxis("Horizontal") * speed;
         float deltaZ = Input.GetAxis("Vertical") * speed;
 
@@ -33,5 +57,16 @@ public class Movement : MonoBehaviour
 
         characterController.Move(movement);
     }
-    
+    private void Dash(KeyCode currentKey, Vector3 movement)
+    {
+        if ((Time.time - clickTime) < dashTime && previousKey == currentKey)
+        {
+            movement.y = gravity;
+            movement = transform.TransformDirection(movement);
+            characterController.Move(movement); 
+        }
+        clickTime = Time.time;
+        previousKey = currentKey;
+    } 
 }
+
